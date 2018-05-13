722a74d1201216b3be09edf242469811452b5e0f
Max <maxnelson997@gmail.com>
2018-05-13 14:09:09 -0600

    created signup component

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,32 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import reducers from './reducers';
+import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 // import 'bootstrap/dist/css/bootstrap.css';
-import './style/main.scss';
+import "./style/main.scss";
 
 //installed components
 
 //our components
-import Home from './components/home';
-
+import Home from "./components/home";
+import Signup from "./components/auth/signup";
 
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
         <Switch>
-          <Route path="/" exact component={Home}></Route>
+          <Route path="/" exact component={Home} />
+          <Route path="/signup" component={Signup} />
         </Switch>
       </BrowserRouter>
-    </Provider>
-    , document.querySelector('.app-wrapper'));
+    </Provider>,
+    document.querySelector(".app-wrapper")
+  );
 }
 
-document.addEventListener('DOMContentLoaded', main);
+document.addEventListener("DOMContentLoaded", main);
