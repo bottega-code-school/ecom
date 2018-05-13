6521a960618a3a363beaf0a6179a447716624ea8
Max <maxnelson997@gmail.com>
2018-05-12 18:32:57 -0600

    setup home component JSX structure

--- src/components/home.js
+++ src/components/home.js
@@ -3,8 +3,35 @@ import React, { Component } from 'react';
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>Gummy Code</h1>
+      <div className="home">
+
+        <div className="header">
+          <img src="http://via.placeholder.com/50x50"/>
+        </div>
+
+        <ul className="navbar">
+          <li>All</li>
+          <li>JavaScript</li>
+          <li>UI/UX</li>
+          <li>Linux</li>
+          <li>Python</li>
+          <li>UML</li>
+          <li>Ruby</li>
+        </ul>
+
+        <div className="search-bar">
+          <input type="text"/>
+        </div>
+
+        <ul className="store">
+          <li>Graph Database</li>
+          <li>Graph Database</li>
+          <li>Graph Database</li>
+          <li>Graph Database</li>
+          <li>Graph Database</li>
+          <li>Graph Database</li>
+        </ul>
+
       </div>
     );
   }
