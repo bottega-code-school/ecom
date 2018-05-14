fca6aebef5e64bcb534a57c793f79ce1febd85f8
Max <maxnelson997@gmail.com>
2018-05-14 14:23:01 -0600

    finished account information grid

--- src/components/userInfo/accountInformationForm.js
+++ src/components/userInfo/accountInformationForm.js
@@ -24,11 +24,47 @@ class AccountInformationForm extends Component {
 
         return (
             <form className="account-information-form" onSubmit={handleSubmit}>
-                <h1 className="account-information-form__register">Register</h1>
+                <h1 className="account-information-form__account-information">Account Information</h1>
                 <div className="account-information-form__name">
                     <label>Name</label>
                     <Field name="name" type="text" component={this.renderInput}/>
                 </div>
+                <div className="account-information-form__email">
+                    <label>Email</label>
+                    <Field name="email" type="email" component={this.renderInput}/>
+                </div>
+                <div className="account-information-form__password">
+                    <label>Current Password</label>
+                    <Field name="password" type="password" component={this.renderInput}/>
+                </div>
+                <div className="account-information-form__new">
+                    <label>New Password</label>
+                    <Field name="new" type="password" component={this.renderInput}/>
+                </div>
+                <div className="account-information-form__confirm">
+                    <label>Confirm Password</label>
+                    <Field name="confirm" type="password" component={this.renderInput}/>
+                </div>
+
+                <div className="account-information-form__street-address">
+                    <label>Street Address</label>
+                    <Field name="street-address" type="text" component={this.renderInput}/>
+                </div>
+                <div className="account-information-form__city">
+                    <label>City</label>
+                    <Field name="city" type="text" component={this.renderInput}/>
+                </div>
+                <div className="account-information-form__state">
+                    <label>State</label>
+                    <Field name="state" type="text" component={this.renderInput}/>
+                </div>
+                <div className="account-information-form__zipcode">
+                    <label>Zipcode</label>
+                    <Field name="zipcode" type="zipcode" component={this.renderInput}/>
+                </div>
+                <div className="account-information-form__line"></div>
+                <Link className="account-information-form__cancel" to="/">Cancel</Link>
+                <button className="account-information-form__button" type="submit">Update Information</button>
             </form>
         )
     }
