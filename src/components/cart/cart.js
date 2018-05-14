4705023b2b9673b46856df138bf79d989360d039
Max <maxnelson997@gmail.com>
2018-05-14 16:08:32 -0600

    debugged layout for cart and put together grid skeletons

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -4,26 +4,29 @@ class Cart extends Component {
     render() {
         return (
             <div className="cart">
+
                 <div className="cart__toggle">
                     <a class="fa">&#xf00d;</a>
                 </div>
+
                 <div className="cart__body">
-                    <div className="cart__title">
+                    <div className="cart__body__title">
                         Cart (4)
                     </div>
-                    <div className="cart__products">
-                        <div className="products">
+                    <div className="cart__body__products">
+                        <div className="cart__body__products__wrapper">
                             This is where a bunch of products will go
                         </div>
                     </div>
-                    <div className="cart__footer">
-                        <div className="cart__footer-checkout">
+                    <div className="cart__body__footer">
+                        <div className="cart__body__footer__checkout">
                             <a>Checkout</a>
                         </div>
-                        <div className="cart__footer-sub">Subtotal</div>
-                        <div className="cart__footer-total">$7.96</div>
+                        <div className="cart__body__footer__subtotal">Subtotal</div>
+                        <div className="cart__body__footer__total">$7.96</div>
                     </div>
                 </div>
+
             </div>
         )
     }
