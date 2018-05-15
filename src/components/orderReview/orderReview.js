dc816199da52d1de5b57b158ba2c9a4c94dea34d
Max <maxnelson997@gmail.com>
2018-05-15 11:36:41 -0600

    built order review component boilerplate

---
+++ src/components/orderReview/orderReview.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class OrderReview extends Component {
+    render() {
+        return (
+            <div className="order-review">
+
+            </div>
+        )
+    }
+}
+
+export default OrderReview;
